9db3d7381e5f1c8cb1729b3bda0565d2ad362b5f
ldco2016 <renaissance.scholar2012@gmail.com>
2019-12-24 06:11:49 -0600

    Dynamic Array Creation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { User } from "./models/User";
 
-const user = new User({});
+const user = new User({ name: "mynameis", age: 20 });
 
-user.set({ age: 999 });
+user.on("change", () => {
+  console.log("Change #1");
+});
+user.on("change", () => {
+  console.log("Change #2");
+});
+user.on("save", () => {
+  console.log("Save was triggered");
+});
 
-console.log(user.get("name"));
-console.log(user.get("age"));
+user.trigger("randomeventname");
