d62914f9f1ab8c247725d94faac36e821cef5eec
ldco2016 <renaissance.scholar2012@gmail.com>
2020-09-24 22:38:54 -0500

    Refactoring Sync

---
+++ src/models/Sync.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosResponse, AxiosPromise } from "axios";
+import { UserProps } from "./User";
+
+("http://localhost:3000/users");
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${"id"}`);
+  }
+
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      // put
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      // post
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
