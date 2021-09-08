6d33c92e13cb8f9fbe1f2d80d02b5696f7ea43cc
ldco2016 <renaissance.scholar2012@gmail.com>
2021-09-08 11:44:01 -0500

    Successfully added an eventing system into my View

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
-const collection = User.buildUserCollection();
+const root: Element = document.getElementById("root")!;
 
-collection.on("change", () => {
-  console.log(collection);
-});
+const userForm = new UserForm(root);
 
-collection.fetch();
+userForm.render();
