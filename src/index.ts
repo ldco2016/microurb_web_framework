3c4415eb54df3c4bd0c039776e32ccb457a009ac
ldco2016 <renaissance.scholar2012@gmail.com>
2021-09-13 23:50:06 -0500

    Added a method to the User Model

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { UserForm } from "./views/UserForm";
+import { User } from "./models/User";
+
+const user = User.buildUser({ name: "NAME", age: 20 });
 
 const root: Element = document.getElementById("root")!;
 
-const userForm = new UserForm(root);
+const userForm = new UserForm(root, user);
 
 userForm.render();
