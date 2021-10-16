2242dbe1ae8f5f1bdc73cd6f4ca33076a426e16f
ldco2016 <renaissance.scholar2012@gmail.com>
2021-10-16 02:52:27 -0500

    extending with generic constraints

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,13 +1,23 @@
-import { User } from "../models/User";
+import { User, UserProps } from "../models/User";
+import { View } from "./View";
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {}
-
+export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       "click:.set-age": this.onSetAgeClick,
+      "click:.set-name": this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector("input");
+
+    if (input) {
+      const name = input.value;
+
+      this.model.set({ name });
+    }
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -19,29 +29,8 @@ export class UserForm {
       <div>User name: ${this.model.get("name")}</div>
       <div>User age: ${this.model.get("age")}</div>
       <input />
-      <button>Click Me</button>
+      <button class="set-name">Change Name</button>
       <button class="set-age">Set Random Age</button>
     </div>`;
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(":");
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  render(): void {
-    const templateElement = document.createElement("template");
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
